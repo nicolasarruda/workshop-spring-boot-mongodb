c319171dda32cc7fb62b75d686b0a8587b30d78a
nicolasarruda <nlarruda@hotmail.com>
2021-10-30 18:19:16 -0300

    Getting a user by id

--- src/main/java/com/nicolasarruda/workshopmongo/services/UserService.java
+++ src/main/java/com/nicolasarruda/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.nicolasarruda.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nicolasarruda.workshopmongo.domain.User;
 import com.nicolasarruda.workshopmongo.repositories.UserRepository;
+import com.nicolasarruda.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,8 @@ public class UserService {
 		return repository.findAll();
 	}
 	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
