f1bc5e22bdfa38d17ff5912ebba8abcab7efca60
nicolasarruda <nlarruda@hotmail.com>
2021-10-31 18:58:04 -0300

    Query with various criteria

--- src/main/java/com/nicolasarruda/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/nicolasarruda/workshopmongo/repositories/PostRepository.java
@@ -1,5 +1,6 @@
 package com.nicolasarruda.workshopmongo.repositories;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -9,10 +10,13 @@ import org.springframework.stereotype.Repository;
 import com.nicolasarruda.workshopmongo.domain.Post;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post, String>{
+public interface PostRepository extends MongoRepository<Post, String> {
 
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
-	
+
 	List<Post> findByTitleContainingIgnoreCase(String text);
+
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: {$lte : ?2} }, { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
